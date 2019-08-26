ce3460fa8d97cc5b77001298141ddc46d919ce6a
Vitor Rodrigues <vitor.rodrigues@socialpoint.es>
2019-08-26 18:03:20 +0200

    More files

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,9 +1,47 @@
-﻿using UnityEngine;
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public Mesh Mesh;
+    public Material Material;
+
+    static GameController _instance;
+
+    public static GameController Instance => _instance;
+
     void Awake()
     {
-        
+        _instance = this;
+
+        var entityManager = World.Active.EntityManager;
+        EntityArchetype entityArchetype = entityManager.CreateArchetype(
+            typeof(Translation),
+            typeof(SpriteSheetAnimation_Data)
+        );
+
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(10000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entityArray);
+
+        foreach (Entity entity in entityArray) {
+            entityManager.SetComponentData(entity,
+                new Translation {
+                    Value = new float3(UnityEngine.Random.Range(-5f, 5f), UnityEngine.Random.Range(-2.5f, 2.5f), 0)
+                }
+            );
+            entityManager.SetComponentData(entity,
+                new SpriteSheetAnimation_Data {
+                    currentFrame = UnityEngine.Random.Range(0, 7),
+                    frameCount = 7,
+                    frameTimer = UnityEngine.Random.Range(0f, 1f),
+                    frameTimerMax = .1f
+                }
+            );
+        }
+
+        entityArray.Dispose();
     }
 }
---
+++ Assets/Scripts/SpriteSheetRenderer.cs
@@ -0,0 +1,19 @@
+﻿/*using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class SpriteSheetRenderer : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation translation) =>
+        {
+            Graphics.DrawMesh(GameController.Instance.Mesh,
+                translation.Value,
+                Quaternion.identity,
+                GameController.Instance.Material,
+                0);
+        });
+    }
+}
+*/
